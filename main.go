a4aae28986ce59b792ba77cd64492973a7b6ddf3
Evgeniy Fedotov <hardpair@yandex.ru>
2024-11-24 20:14:01 +0300

    go-db-sql-final (stage1)

--- main.go
+++ main.go
@@ -98,8 +98,14 @@ func (s ParcelService) Delete(number int) error {
 
 func main() {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
 
-	store := // создайте объект ParcelStore функцией NewParcelStore
+	store := NewParcelStore(db) // создайте объект ParcelStore функцией NewParcelStore
 	service := NewParcelService(store)
 
 	// регистрация посылки
@@ -139,6 +145,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	// дальше процеес не пойдет  ибо обломалось выше если прикрыть return, досмотрим кино до конца.
 
 	// вывод посылок клиента
 	// предыдущая посылка не должна удалиться, т.к. её статус НЕ «зарегистрирована»
